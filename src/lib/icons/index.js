edbcbf1a64a089f8f5b6716fe1c658d2f09868b6
dhernando <david.hernando91@gmail.com>
2025-02-03 07:54:39 +0100

    prettier

--- src/lib/icons/index.js
+++ src/lib/icons/index.js
@@ -1,9 +1,5 @@
-import Close from "$lib/icons/Close.svelte"
-import Cog from "$lib/icons/Cog.svelte"
-import Trash from "$lib/icons/Trash.svelte"
+import Close from "$lib/icons/Close.svelte";
+import Cog from "$lib/icons/Cog.svelte";
+import Trash from "$lib/icons/Trash.svelte";
 
-export {
-  Close,
-  Cog,
-  Trash,
-}
+export { Close, Cog, Trash };
