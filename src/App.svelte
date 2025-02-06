5ccfe1530ca72b26a85d17d642e4066b9b55c556
dhernando <david.hernando91@gmail.com>
2025-02-06 14:11:13 +0100

    prettier

--- src/App.svelte
+++ src/App.svelte
@@ -22,10 +22,8 @@
       const tab = await activeTab();
       const url = new URL(tab.url);
 
-      const hosts = [{ value: "github.com" }, ...enterpriseHosts]
-      const hasPRLink = hosts.some(
-        (host) => url.hostname === host.value && isPRLink(url.toString()),
-      );
+      const hosts = [{ value: "github.com" }, ...enterpriseHosts];
+      const hasPRLink = hosts.some((host) => url.hostname === host.value && isPRLink(url.toString()));
 
       copyEnabled = hasPRLink;
     })();
